6061016e2c05d36f579f6a0d54fac7d662fe78bf
Torolfr <torolfr@yandex.ru>
2024-05-13 17:15:43 +0300

    k8s update

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -13,8 +13,6 @@ terraform {
     bucket = "momo-store-terraform-state"
     region = "ru-central1"
     key    = "terraform.tfstate"
-    access_key = ""
-    secret_key = ""
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true

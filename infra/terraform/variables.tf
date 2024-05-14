9f77651355299bb826042f91ff5075a16395482b
std-024-29 std-024-29 <std-024-29@praktikum-services.ru>
2024-05-14 09:12:04 +0000

    Terraform

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "token" {
+  description = "Yandex Cloud token"
+  type        = string
+  sensitive   = true
+}
+
+variable "cloud_id" {
+  description = "Yandex cloud_id"
+  type        = string
+  default     = "b1g3el3r09to8on5mmso"
+}
+
+variable "folder_id" {
+  description = "Yandex folder_id"
+  type        = string
+  default     = "b1g1h2m7591qu55h34jf"
+}
+
+variable "zone" {
+  description = "Yandex zone"
+  type        = string
+  default     = "ru-central1-a"
+}

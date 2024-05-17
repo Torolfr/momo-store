17f2466f0f1ec6c57c5a967c0a91dbd4250d5a1b
Torolfr <torolfr@yandex.ru>
2024-05-17 16:16:01 +0300

    k8s plugins

--- infra/kubernetes/cert-manager.sh
+++ infra/kubernetes/cert-manager.sh
@@ -1,1 +1,12 @@
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
+export HELM_EXPERIMENTAL_OCI=1 && \
+helm pull oci://cr.yandex/yc-marketplace/yandex-cloud/cert-manager-webhook-yandex/cert-manager-webhook-yandex \
+  --version 1.0.8-1 \
+  --untar && \
+helm install \
+  --namespace <пространство_имен> \
+  --create-namespace \
+  --set-file config.auth.json=key.json \
+  --set config.email='<адрес_электронной_почты_для_уведомлений_от_Lets_Encrypt>' \
+  --set config.folder_id='<идентификатор_каталога_с_зоной_Cloud_DNS>' \
+  --set config.server='https://acme-v02.api.letsencrypt.org/directory' \
+  cert-manager-webhook-yandex ./cert-manager-webhook-yandex/
